f3cbb646c3fd0569bd1bf311f75894b1f71e178e
Princelo <lamkimcheung@gmail.com>
2015-09-08 16:46:11 +0800

    formula

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -49,7 +49,8 @@ class Welcome extends CI_Controller {
             [$this->session->userdata('biz_id')]);
         $auth_data = $query->result()[0];
         $view_data['formula_local_score'] = $this->db->query("
-                    select sum(lscore) total_score from fanwe_biz_consume_log where biz_id = ?
+                    select sum(score) total_score from fanwe_biz_consume_log where biz_id = ?
+                    and biz_id != pid
                     and unix_timestamp(create_time) >=
                         case when (select max(unix_timestamp(create_time)) from fanwe_settle_biz_log where biz_id = ?)
                         is null then 0 else (select max(unix_timestamp(create_time)) from fanwe_settle_biz_log where biz_id = ?)
