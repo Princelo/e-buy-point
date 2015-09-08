889714950ce74ef558826859c4e8178980041708
Princelo <lamkimcheung@gmail.com>
2015-09-08 16:38:02 +0800

    formula

--- application/models/Report_model.php
+++ application/models/Report_model.php
@@ -192,7 +192,7 @@ class Report_model extends CI_Model {
             select
                 l.create_time time,
                 l.title, l.remark, l.consumer_name, l.consumer_id, l.volume, l.ratio
-                ,s.name, u.user_name, l.score, l.type
+                ,s.name, u.user_name, l.score, l.type, l.*
             from ".DB_PREFIX."biz_consume_log l,".DB_PREFIX."user u,".DB_PREFIX."supplier_location s
             where 1 = 1
             and s.id = u.p_biz_id
